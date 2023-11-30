28b115291fba3c86637bd935d43b9a52c6b6734b
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2023-11-30 21:48:38 +0100

    Test du scroll

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -2,7 +2,9 @@
 
 import { ThemeContext } from "@/context/ThemeContext";
 import React, { useContext } from "react";
-import { yeseva_One } from "@/fonts/fonts";
+import Link from "next/link";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const HomeSection = () => {
   const { theme } = useContext(ThemeContext);
@@ -10,7 +12,7 @@ const HomeSection = () => {
   return (
     <section
       id="accueil"
-      className="pt-[5rem] h-screen -mt-0"
+      className="py-[5rem] "
       style={
         theme === "dark"
           ? { backgroundColor: "#0f172a" }
@@ -25,16 +27,53 @@ const HomeSection = () => {
             className="rounded-full w-[30%] h-[30%] animate-move max-[768px]:w-[50%] max-[768px]:h-[50%] max-[425px]:w-[70%] max-[425px]:h-[70%] "
           />
           <div className="flex flex-col justify-center max-[768px]:items-center  ">
-            <span className="text-[22px] text-[#7c8587] ">
-              Salut, je suis{" "}
-            </span>
-            <h2
-              className="text-[40px] text-white max-[425px]:text-[28px] "
-              
-            >
-              Eric Colday Noubissi 
+            <span className="text-[22px] text-[#7c8587] ">Salut, je suis </span>
+            <h2 className="text-[40px] text-white max-[425px]:text-[28px] ">
+              Eric Colday Noubissi
             </h2>
-            <span className="text-[50px] font-extrabold text-[#7c8587] max-[768px]:text-center max-[425px]:text-[40px] " >Développeur <br/> Front-end</span>
+            <span className="text-[50px] font-extrabold text-[#7c8587] max-[768px]:text-center max-[425px]:text-[40px] ">
+              Développeur <br /> Front-end
+            </span>
+            <div className="flex gap-[20px] ">
+              <Link
+                href="https://github.com/eric-colday"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Github"
+              >
+                <GitHubIcon className="text-[#2f3854] cursor-pointer hover:text-[#7c8587] " />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/ecnoubissi/"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+              >
+                <LinkedInIcon className="text-[#2f3854] cursor-pointer hover:text-[#7c8587] " />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center gap-[20px] max-[375px]:flex-col ">
+          <div>Tech Stack</div>
+          <div className="max-[375px]:hidden ">| </div>
+          <div className="hidden max-[375px]:block "> _____ </div>
+          <div className="flex gap-[20px]">
+            <img
+              src="./img/icons.svg"
+              alt="logo html et css"
+              className="w-[50px] h-[50px] "
+            />
+            <img
+              src="./img/icons2.svg"
+              alt="logo html et css"
+              className="w-[50px] h-[50px] "
+            />
+            <img
+              src="./img/icons3.svg"
+              alt="logo html et css"
+              className="w-[50px] h-[50px] "
+            />
           </div>
         </div>
       </div>
