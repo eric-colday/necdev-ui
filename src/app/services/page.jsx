ecfe4793838e5661893572f7ccc19f27a1d03d7d
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-07-28 00:40:16 +0200

    Modif du background des pages

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+"use client";
+
+import React, { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 
 function Services() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div>
-      Services
-    </div>
-  )
+    <main>
+      <section
+        className="pt-36 "
+        style={
+          theme === "dark"
+            ? { backgroundColor: "#0f172a" }
+            : { backgroundColor: "#0f172a" }
+        }
+      >
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="mb-4 text-4xl font-bold">Services</h1>
+          <div className="mb-10 w-10 border-2 border-[#7c8587]"></div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default Services
+export default Services;
