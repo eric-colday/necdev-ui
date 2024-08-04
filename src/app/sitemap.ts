7de67c2343c0a0507de44470ebda689edeb312f6
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-08-04 21:51:25 +0200

    Mise à jour du sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,5 +6,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             url: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
             lastModified: new Date().toISOString(),
         },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/qui-suis-je`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_SITE_URL}/services`,
+            lastModified: new Date().toISOString(),
+        },
     ]
 }
