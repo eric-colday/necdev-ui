20a6bf80eeb5f6998fb0ec6bde55febdfb439289
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-08-04 21:47:53 +0200

    Mise à jour de la page services

--- src/components/Services/ServicesList.jsx
+++ src/components/Services/ServicesList.jsx
@@ -4,6 +4,7 @@ import React from "react";
 export default function ServicesList() {
   return (
     <section className="py-40 px-6 max-[320px]:px-4">
+      {/* SEO */}
       <div className="flex max-[920px]:flex-col-reverse  justify-center items-center gap-10 ">
         <div className="w-[500px] max-[920px]:w-auto max-[920px]:text-center">
           <h1 className="mb-4 text-[50px] font-bold">SEO</h1>
@@ -22,6 +23,68 @@ export default function ServicesList() {
             width="500"
             height="300"
           />
+        </div>
+      </div>
+      {/* SEA */}
+      <div className="flex max-[920px]:flex-col justify-center items-center gap-10 mt-20">
+        <div className="w-[500px] max-[920px]:w-auto  ">
+          <Image
+            src="/img/sea-nec.jpg"
+            alt="services"
+            width="500"
+            height="300"
+          />
+        </div>
+        <div className="w-[500px] max-[920px]:w-auto max-[920px]:text-center">
+          <h1 className="mb-4 text-[50px] font-bold">SEA</h1>
+          <p className="text-[20px] w-[500px] max-[539px]:w-auto max-[539px]:p-4 ">
+            Le
+            <strong> SEA</strong> est un levier incontournable pour générer du
+            trafic qualifié sur votre site. Je vous accompagne dans la mise en
+            place de campagnes Google Ads, Facebook/Instagram Ads et linkedIn
+            Ads.
+          </p>
+        </div>
+      </div>
+      {/* Analytics */}
+      <div className="flex max-[920px]:flex-col-reverse  justify-center items-center gap-10 mt-20">
+        <div className="w-[500px] max-[920px]:w-auto max-[920px]:text-center">
+          <h1 className="mb-4 text-[50px] font-bold">Analytics</h1>
+          <p className="text-[20px] w-[500px] max-[539px]:w-auto max-[539px]:p-4 ">
+            L'
+            <strong> Analytics</strong> est un outil indispensable pour mesurer
+            les performances de votre site. Je vous aide à configurer Google
+            Analytics et Google Tag Manager pour suivre les performances de
+            votre site.
+          </p>
+        </div>
+        <div className="w-[500px] max-[920px]:w-auto  ">
+          <Image
+            src="/img/seo-nec.jpg"
+            alt="services"
+            width="500"
+            height="300"
+          />
+        </div>
+      </div>
+      {/* Création de site */}
+      <div className="flex max-[920px]:flex-col justify-center items-center gap-10 mt-20">
+        <div className="w-[500px] max-[920px]:w-auto  ">
+          <Image
+            src="/img/sea-nec.jpg"
+            alt="services"
+            width="500"
+            height="300"
+          />
+        </div>
+        <div className="w-[500px] max-[920px]:w-auto max-[920px]:text-center">
+          <h1 className="mb-4 text-[50px] font-bold">Création de site</h1>
+          <p className="text-[20px] w-[500px] max-[539px]:w-auto max-[539px]:p-4 ">
+            La
+            <strong> création de site</strong> est une étape importante pour
+            votre visibilité en ligne. Je vous accompagne dans la création de
+            votre site vitrine ou e-commerce.
+          </p>
         </div>
       </div>
     </section>
