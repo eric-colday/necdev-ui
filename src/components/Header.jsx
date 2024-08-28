aa5599abffa1e75bf4952bc9c3d840d9318c3f09
Éric  Colday NOUBISSI <54113095+eric-colday@users.noreply.github.com>
2024-08-28 20:30:01 +0200

    vitesse de chargement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default async function Header() {
 
   return (
     <div className="bg-[#0f172a] fixed top-0 left-0 right-0 z-50 flex justify-around items-center h-[80px] gap-[100px] border-b-[1px] border-[#468bff] max-[375px]:gap-[50px]">
-      <Link href="/" className="font-bold  text-[40px] text-[#468bff] p-2 ">
+      <Link href="/" className="font-bold  text-[40px] text-[#468bff] p-2 max-[425px]:text-[25px] ">
         <div>NEC</div>
       </Link>
       <div className="flex justify-center items-center gap-[20px] text-[16px]">
